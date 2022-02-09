d7e0ce6d6698fccf8e5471a5c513fd4d1528cfbe
JXCoding <hsvbuh12@gmail.com>
2022-02-09 23:39:39 +0100

    Inital commit

---
+++ src/main/java/de/jason/economywars/Economywars.java
@@ -0,0 +1,17 @@
+package de.jason.economywars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Economywars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+
+
+}
